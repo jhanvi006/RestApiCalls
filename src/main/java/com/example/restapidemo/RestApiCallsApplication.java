ae70d831a5feb776fc80b4068f85e54dcfe4a7a3
jhanvi006 <jhanvikankhara06@gmail.com>
2022-07-06 02:33:55 +0530

    Added: welcome message added.

---
+++ src/main/java/com/example/restapidemo/RestApiCallsApplication.java
@@ -0,0 +1,13 @@
+package com.example.restapidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiCallsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiCallsApplication.class, args);
+        System.out.println("Hello!");
+    }
+}
